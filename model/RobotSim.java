2823bb3453864597c9c0904e63560db855bc331b
Joakim Ysing <joakim.ysing@gmail.com>
2017-02-24 16:07:05 +0100

    Added RobotSim class

---
+++ model/RobotSim.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.awt.*;
+import java.util.*;
+
+import control.EstimatorInterface;
+
+public class RobotSim {
+
+	// todo
+	public RobotSim() {
+
+	}
+
+	// todo
+	public int[] getCurrentPos() {
+		return null;
+	}
+
+	// todo
+	public void getCurrentReading() {
+
+	}
+
+	// todo
+	public void move() {
+
+	}
+}
